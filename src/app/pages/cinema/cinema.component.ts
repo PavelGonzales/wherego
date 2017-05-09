01561c6273c8dcdf5722d299bff6804a38d249aa
Pavel Gonzales <develop.gonzales@gmail.com>
2017-05-09 18:11:20 +0300

    начал выводить постер по клику, ура

--- src/app/pages/cinema/cinema.component.ts
+++ src/app/pages/cinema/cinema.component.ts
@@ -7,12 +7,29 @@ import { CinemaService } from './../../services/cinema.service';
   styleUrls: ['./cinema.component.styl']
 })
 export class CinemaComponent implements OnInit {
+  title = 'title';
+  poster = null;
+  movies = [];
 
   constructor(private cinemaService: CinemaService) {
+    console.log(this.movies);
   }
 
-  ngOnInit() {
-    console.log(this.cinemaService.getData());
+  ngOnInit(){
+    this.cinemaService.getData().subscribe((movies) => {
+      console.log('movies в subscribe', movies)
+      this.movies = movies;
+      // this.title = this.movies[0].title;
+    });
+    // console.log('movies', this.cinemaService.getData());
+    console.log('movies', this.movies);
+    
   }
+
+  showMovie() {
+    this.poster = this.movies[0].poster;
+    this.title = this.movies[0].title;
+  }
+
 
 }
--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -3,20 +3,28 @@ import { Http } from '@angular/http';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class CinemaService {
-
   constructor(private http: Http) { }
 
   getData() {
-    this.http.get('//gonzalez-kudago.betaagency.ru/public-api/v1.3/movies/?location=msk&fields=id,title,body_text,poster&page_size=100')
-      .toPromise()
-      .then((response) => {
-        console.log('Все отлично =>', response.json().results);
-      }, (error) => {
-        console.log('Ошибка =>', error);
-      });
+    return this.http.get('//gonzalez-kudago.betaagency.ru/public-api/v1.3/movies/?location=msk&fields=id,title,body_text,poster&page_size=100')
+      .map((response:Response)=>{
+        // console.log('Возврат из сервиса', response.json().results);
+        // return response.json().results;
+
+        let cinemaList = response.json().results;
+        let movies = [];
+        for(let index in cinemaList){
+            // console.log(cinemaList[index]);
+            let movie = cinemaList[index];
+            movies.push({ title: movie.title, poster: movie.poster.image });
+        }
+        console.log('cinemaList из сервиса перед return', movies)
+        return movies;
+
+
+    });
   }
 }
